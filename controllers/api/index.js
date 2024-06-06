82aaa42ce9762fce8cc37cd3b7c0518ec799c682
Jake Rasmusson <jakerasmusson@gmail.com>
2024-06-05 21:44:50 -0500

    spotterroutes validation

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
-const userRoutes = require('./userRoutes');
+const spotterRoutes = require('./spotterRoutes');
 
-router.use('/users', userRoutes);
+router.use('/spotters', spotterRoutes);
 
 module.exports = router;
--- controllers/viewRoutes.js
+++ controllers/viewRoutes.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
-const { User } = require('../models');
+const { Spotter } = require('../models');
 const withAuth = require('../utils/auth');
 
-router.get('/', withAuth, async (req, res) => {
+router.get('/', async (req, res) => {
   try {
     res.render('homepage', {
       logged_in: req.session.logged_in,
