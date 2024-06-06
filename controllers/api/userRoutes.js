69451df9f8e6d5cf17fd72d9ff885e11fcebc0a9
Jake Rasmusson <jakerasmusson@gmail.com>
2024-06-05 21:08:07 -0500

    create user route

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,18 +1,33 @@
 const router = require('express').Router();
-const { User } = require('../../models');
+const { Spotter } = require('../../models');
+
+router.post('/', async (req, res) => {
+  try {
+    const createSpotter = await Spotter.create(
+      req.body
+    )
+    res.status(200).send('User created succesfully')
+  } catch (err) {
+    res.status(400).send('Server error creating user');
+  }
+})
+
+
+
+
 
 router.post('/login', async (req, res) => {
   try {
-    const userData = await User.findOne({ where: { email: req.body.email } });
+    const spotterData = await Spotter.findOne({ where: { email: req.body.email } });
 
-    if (!userData) {
+    if (!spotterData) {
       res
         .status(400)
         .json({ message: 'Incorrect email or password, please try again' });
       return;
     }
 
-    const validPassword = await userData.checkPassword(req.body.password);
+    const validPassword = await spotterData.checkPassword(req.body.password);
 
     if (!validPassword) {
       res
@@ -22,10 +37,10 @@ router.post('/login', async (req, res) => {
     }
 
     req.session.save(() => {
-      req.session.user_id = userData.id;
+      req.session.Spotter_id = spotterData.id;
       req.session.logged_in = true;
       
-      res.json({ user: userData, message: 'You are now logged in!' });
+      res.json({ Spotter: spotterData, message: 'You are now logged in!' });
     });
 
   } catch (err) {
