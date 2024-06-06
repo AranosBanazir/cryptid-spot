5530f7a382cfef3feee3fc6e6bfb7852c7d32354
Jake Rasmusson <jakerasmusson@gmail.com>
2024-06-05 19:53:58 -0500

    model relationships

--- models/index.js
+++ models/index.js
@@ -1,3 +1,32 @@
-const User = require('./User');
+const Spotter = require('./Spotter');
+const Cryptid = require('./Cryptid');
+const Sighting = require('./Sighting')
 
-module.exports = { User };
+//Relationships
+
+Spotter.hasMany(Sighting, {
+    foreignKey: 'spotter_id'
+})
+Sighting.belongsTo(Spotter, {
+    foreignKey: 'spotter_id'
+})
+//
+
+Spotter.hasMany(Cryptid, {
+    foreignKey: 'spotter_id'
+})
+Cryptid.belongsTo(Spotter, {
+    foreignKey: 'spotter_id'
+})
+
+//
+Cryptid.hasMany(Sighting, {
+    foreignKey: 'cryptid_id'
+})
+Sighting.belongsTo(Cryptid, {
+    foreignKey: 'cryptid_id'
+})
+
+
+
+module.exports = { Spotter, Cryptid, Sighting };
