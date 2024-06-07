0c51814041e48009faca726bbda61359ee3da13e
LerieLogin <161064384+LerieLogin@users.noreply.github.com>
2024-06-07 02:25:08 -0500

    added library bars and js and changed main bars

--- utils/helpers.js
+++ utils/helpers.js
@@ -11,4 +11,11 @@ module.exports = {
 
     return `<span for="img" aria-label="book">${book}</span>`;
   },
+  format_date: function(date) {
+    const year = date.getFullYear()
+    const month = date.getMonth() + 1
+    const day = date.getDay()
+
+    return `${month}/${day}/${year}`
+  }
 };
