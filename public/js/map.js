f2fdea530096fce7efff5565c3bbdda688d20492
LerieLogin <161064384+LerieLogin@users.noreply.github.com>
2024-06-14 01:19:39 -0500

    small map,js changes

--- public/js/map.js
+++ public/js/map.js
@@ -53,9 +53,11 @@ const setMarker = async (lat, lon, cryptid, spottedDate = "at some point.", spot
   });
 
     const contentStr = `
+    <div class='bg-sky-300 rounded-md	p-5'>
+    <p class='text-black text-xl font-bold my-3'>${content}</p>
+    <h2 style='font-size: 20px;' class='text-black my-3'>${spotter} spotted ${cryptid} here on ${spottedDate}</h2>
     <img src='${img}' class='rounded'>
-    <p class='text-black text-xl'>${content}</p>
-    <h2 style='font-size: 12px;' class='text-black'>${spotter} spotted ${cryptid} here on ${spottedDate}</h2>
+    </div>
     `
 
 
